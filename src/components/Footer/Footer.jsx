8d17191379feba43815e883ff96ad36b2e9071a5
reetiv <reetiverma21@gmail.com>
2026-04-03 20:09:22 +0530

    Fix typo in className attribute for footer logo

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
     <div className='footer' id='footer'>
         <div className="footer-content">
             <div className="footer-content-left">
-                <img src={assets.logo} classname="footer-logo" alt="" />
+                <img src={assets.logo} className="footer-logo" alt="" />
                 <p></p>
                 <div className="footer-social-icons">
                     <img src={assets.facebook_icon} alt="" />
